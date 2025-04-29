f1745eef41c9482c22c6d05c35182dc006b18faa
Emily L. Balestrin <emilyl.balestrin@gmail.com>
2025-04-29 18:11:51 -0300

    docs: adiciona configuração inicial e bibliotecas

---
+++ corrida.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <omp.h>    // Biblioteca para paralelismo com OpenMP
+#include <vector>
+#include <unistd.h>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+
